6f08c263f79786d7165600d597a67cd3620e099e
Vladimir Salin <salinvs@gmail.com>
2018-07-26 23:29:53 +0100

    add real sh to run crawlers

--- runcrawlers.sh
+++ runcrawlers.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-echo "Hi there"
-/usr/local/bin/memorious memorious list
+/usr/local/bin/memorious run au_dfat_sanctions
+/usr/local/bin/memorious run ch_seco_sanctions
+/usr/local/bin/memorious run coe_assembly
+/usr/local/bin/memorious run eu_eeas_sanctions
+/usr/local/bin/memorious run eu_meps
+/usr/local/bin/memorious run eu_whoiswho
+/usr/local/bin/memorious run everypolitician
+/usr/local/bin/memorious run gb_coh_disqualified
+/usr/local/bin/memorious run gb_hmt_sanctions
+/usr/local/bin/memorious run interpol_red_notices
+/usr/local/bin/memorious run kg_fiu_national
+/usr/local/bin/memorious run ua_sdfm_blacklist
+/usr/local/bin/memorious run un_sc_sanctions
+/usr/local/bin/memorious run us_bis_denied
+/usr/local/bin/memorious run us_cia_world_leaders
+/usr/local/bin/memorious run us_ofac
+/usr/local/bin/memorious run worldbank_debarred
+/usr/local/bin/memorious run worldpresidentsdb
