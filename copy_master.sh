6edbc20d150a0585a2e1c97a569ef6f7f8e9c7e9
Vladimir Salin <salinvs@gmail.com>
2018-07-31 18:28:18 +0100

    use the same target name every time when copied

--- copy_master.sh
+++ copy_master.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 OS_LATEST=$(ls -lat /home/ec2-user/opensanctions/data/json | head -2 | tail -1 | awk '{print $9}')
 echo $OS_LATEST
-scp -P 23000 /home/ec2-user/opensanctions/data/json/$OS_LATEST Swiftdfeed@agilertech.com:/home/Swiftdfeed/opensanctions
+scp -P 23000 /home/ec2-user/opensanctions/data/json/$OS_LATEST Swiftdfeed@agilertech.com:/home/Swiftdfeed/opensanctions/master.json
