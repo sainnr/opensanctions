55f6a9f164cc5fe5d8235e07dcb562afd50ce924
Vladimir Salin <salinvs@gmail.com>
2018-08-01 07:47:31 +0100

    zip master file; also remove old ones

--- copy_master.sh
+++ copy_master.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 OS_LATEST=$(ls -lat /home/ec2-user/opensanctions/data/json | head -2 | tail -1 | awk '{print $9}')
 echo $OS_LATEST
-scp -P 23000 /home/ec2-user/opensanctions/data/json/$OS_LATEST Swiftdfeed@agilertech.com:/home/Swiftdfeed/opensanctions/master.json
+zip master.zip $OS_LATEST
+scp -P 23000 /home/ec2-user/opensanctions/data/json/master.zip Swiftdfeed@agilertech.com:/home/Swiftdfeed/opensanctions/master.zip
+ls -d -1 /home/ec2-user/opensanctions/data/json/*.* | grep -v $OS_LATEST | xargs sudo rm
