b19b0573d66d4412682a45d03f00e6b4383b1b75
Vladimir Salin <salinvs@gmail.com>
2018-08-02 20:59:30 +0100

    use a shared access dir to store master zip

--- copy_master.sh
+++ copy_master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 OS_LATEST=$(ls -lat /home/ec2-user/opensanctions/data/json | head -2 | tail -1 | awk '{print $9}')
 echo $OS_LATEST
-zip master.zip $OS_LATEST
-scp -P 23000 /home/ec2-user/opensanctions/data/json/master.zip Swiftdfeed@agilertech.com:/home/Swiftdfeed/opensanctions/master.zip
+zip /var/opensanctions/master.zip /home/ec2-user/opensanctions/data/json/$OS_LATEST
+scp -P 23000 /var/opensanctions/master.zip Swiftdfeed@agilertech.com:/home/Swiftdfeed/opensanctions/master.zip
 ls -d -1 /home/ec2-user/opensanctions/data/json/*.* | grep -v $OS_LATEST | xargs sudo rm
