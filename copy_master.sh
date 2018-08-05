8792918c8e2012af6d05d925109175d4ee66cedf
Vladimir Salin <salinvs@gmail.com>
2018-08-05 22:08:07 +0100

    remove old archive & skip paths inside it

--- copy_master.sh
+++ copy_master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 OS_LATEST=$(ls -lat /home/ec2-user/opensanctions/data/json | head -2 | tail -1 | awk '{print $9}')
 echo $OS_LATEST
-zip /var/opensanctions/master.zip /home/ec2-user/opensanctions/data/json/$OS_LATEST
+rm -f /var/opensanctions/master.zip && zip -j /var/opensanctions/master.zip /home/ec2-user/opensanctions/data/json/$OS_LATEST
 scp -P 23000 /var/opensanctions/master.zip Swiftdfeed@agilertech.com:/home/Swiftdfeed/opensanctions/master.zip
 ls -d -1 /home/ec2-user/opensanctions/data/json/*.* | grep -v $OS_LATEST | xargs sudo rm
