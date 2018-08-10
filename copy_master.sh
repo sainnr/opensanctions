d9810fdd7f79ff699fd47b20a096d69b45b1a39a
Vladimir Salin <salinvs@gmail.com>
2018-08-10 21:31:23 +0100

    stick to master.json name

--- copy_master.sh
+++ copy_master.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 OS_LATEST=$(ls -lat /home/ec2-user/opensanctions/data/json | head -2 | tail -1 | awk '{print $9}')
 echo $OS_LATEST
-rm -f /var/opensanctions/master.zip && zip -j /var/opensanctions/master.zip /home/ec2-user/opensanctions/data/json/$OS_LATEST
+cp /home/ec2-user/opensanctions/data/json/$OS_LATEST /var/opensanctions/master.json
+rm -f /var/opensanctions/master.zip && zip -j /var/opensanctions/master.zip /var/opensanctions/master.json
 scp -P 23000 /var/opensanctions/master.zip Swiftdfeed@agilertech.com:/home/Swiftdfeed/opensanctions/master.zip
 ls -d -1 /home/ec2-user/opensanctions/data/json/*.* | grep -v $OS_LATEST | xargs sudo rm
